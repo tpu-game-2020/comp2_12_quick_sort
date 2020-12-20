e6bc2ba0b1169ed85b6cfe169bfc52702a4f11bd
TairikuS <g2027022@st.t-kougei.ac.jp>
2020-12-20 23:05:38 +0900

    クイックソートを実装しました。

--- src/StaticLib/sort.c
+++ src/StaticLib/sort.c
@@ -2,14 +2,50 @@
 #include <stdbool.h>
 #include "../include/libsort.h"
 
+static void swap(node* p1, node* p2)
+{
+	node tmp;
+
+	tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+static void quick_sort_sub(node * p,int begin, int end)
+{
+	if (begin >= end)
+		return;
+
+	int pivot = p[begin].key;
+	int l = begin;
+	int r = end;
+
+	while (1)
+	{
+		while (p[l].key < pivot)
+			l++;
+
+		while (p[r].key > pivot)
+			r--;
+
+		if (l >= r)
+			break;
+
+		swap(&p[r], &p[l]);
+
+		l++;
+		r--;
+	}
+
+	quick_sort_sub(p, begin, l - 1);
+	quick_sort_sub(p, r + 1, end);
+}
 
 // ソート
 void quick_sort(array* a)
 {
-	// ToDo: 実装してください
-	a->nodes = NULL;
+	quick_sort_sub(a->nodes, 0, a->num - 1);
 }
-
 
 //
 // 配列の初期化
