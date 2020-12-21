16a2b9add1367c19fc57e4841d46c3296d754e5c
isigurohatune <39hatune.1925@gmail.com>
2020-12-21 16:35:58 +0900

    quick sortを実装しました。

--- src/StaticLib/sort.c
+++ src/StaticLib/sort.c
@@ -2,12 +2,43 @@
 #include <stdbool.h>
 #include "../include/libsort.h"
 
+static void swap(node*p1,node*p2)
+{
+	node tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+
+static void quick_sotr_sub(node* p,int begin, int end)
+{
+	int l = begin;
+	int r = end - 1;
+	int pivot;
+	pivot = p[begin].key;
+		
+	if (begin >= end) 
+	{
+		return;
+	}
+
+	while (l < r) {
+		while (p[l].key < pivot)l++;
+		while (p[r].key > pivot)r--;
+
+		swap(&p[l], &p[r]);
+	}
+	quick_sotr_sub(p, begin, 1 - 1);
+	quick_sotr_sub(p, r + 1, end);
+}
+
 
 // ソート
 void quick_sort(array* a)
 {
 	// ToDo: 実装してください
-	a->nodes = NULL;
+	quick_sotr_sub(a->nodes,0, a->num);
+	
 }
 
 
