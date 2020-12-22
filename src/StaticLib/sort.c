26c2778b3769ec6dbe1dc9e594319c3293d2b89d
shuhei-matsushima <shuhei-matsushima@outlook.jp>
2020-12-22 16:27:21 +0900

    実装しようとしましたができませんでした。ご確認お願い致します。

--- src/StaticLib/sort.c
+++ src/StaticLib/sort.c
@@ -2,12 +2,53 @@
 #include <stdbool.h>
 #include "../include/libsort.h"
 
+static void swap(node* p1, node* p2)
+{
+	node tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+static void quick_sort_sub(node* p, int begin, int end)
+{
+	/* ソートを行う範囲が１以下の場合 */
+	if (begin >= end) 
+	{
+		return;
+	}
+	
+	//int pivot;
+	/* pivot の決定 */
+	node pivot = p[begin];
+	int i = begin;
+	int j = end;
+
+	while (1)
+	{
+		while (p[i].key < pivot.key)
+		{
+			i++;
+		}
+		while (p[j].key > pivot.key)
+		{
+			j--;
+		}
+		if (i >= j)
+		{
+			break;
+		}
+		swap(&(p[i]), &(p[j]));
+		i++;
+		j--;
+	}
+	quick_sort_sub(p, begin, i - 1);
+	quick_sort_sub(p, j + 1, end);
+}
 
 // ソート
 void quick_sort(array* a)
 {
-	// ToDo: 実装してください
-	a->nodes = NULL;
+	quick_sort_sub(a->nodes, 0, a->num);
 }
 
 
